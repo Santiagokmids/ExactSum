137221622761715be8e838f84d0aae972fd5930c
Santiago <sntgtrochez@gmail.com>
2021-03-10 22:03:48 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,66 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 	
-	private final static String SEPARATOR = ""; 
+	private final static String SEPARATOR = " "; 
+	private static double[] allPrices;
 	
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws IOException {
+		startApp();
 	}
 	
-	public static void startApp() {
+	public static void startApp() throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
-		
-		try {
 
-			String books = br.readLine();
-			String prices = "";
-			
-			if(books != null){
-				
-				int cause = Integer.parseInt(books);
-				
-				if(cause >= 2 && cause <= 10000) {
-					
-					double[] allPrices = new double[cause];  
+		String books = br.readLine();
+		String prices = "";
+		//String money = "";
 
-					for (int i = 0; i < allPrices.length; i++) {
-						
-						prices = br.readLine();
-						
-						String parts[] = prices.split(SEPARATOR);
-						
-						for (int j = 0; j < parts.length; j++) {
+		if(books != null){
+
+			int cause = Integer.parseInt(books);
+
+			int[] allPrices = new int[cause];
+			prices = br.readLine();
+			String parts[] = prices.split(SEPARATOR);
 							
-							if(Double.parseDouble(parts[j]) < 1000001) {
-								allPrices[i] = Double.parseDouble(parts[j]);
-							}
-						}
+			for (int j = 0; j < parts.length; j++) {
 
-						bw.write();
-					}	
-					br.close();
-					bw.close();
-				}
+				allPrices[j] = Integer.parseInt(parts[j]);
+
 			}
 
-		} catch (NumberFormatException nfe) {
-			System.out.println("\nPlease enter numbers");	
+			bw.write(Arrays.toString(allPrices));
+
+			//money = br.readLine();
+
+			br.close();
+			bw.close();
 		}
 	}
+	
+	/*public static int findSums() {
+		
+		double sum = allPrices[0] + allPrices[1];
+		
+		for (int i = 0; i < allPrices.length; i++) {
+			
+			for (int j = 1; j < allPrices.length; j++) {
+				
+				if(sum < allPrices[i] + allPrices[j]) {
+					
+				} 
+				sum = allPrices[i] + allPrices[j];
+			}
+		}
+		return 0;
+	}*/
 
 }
