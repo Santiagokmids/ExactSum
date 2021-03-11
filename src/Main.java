7f970bb2627d496cea8585922012b4dab2732f86
Santiago <sntgtrochez@gmail.com>
2021-03-11 17:04:35 -0500

    Wihtout eof

--- src/Main.java
+++ src/Main.java
@@ -38,9 +38,9 @@ public class Main{
 			}
 			
 			sort();
-			String money = br.readLine();
-			
-			bw.write(allPrices.toString());
+			String peterMoney = br.readLine();
+			int money = Integer.parseInt(peterMoney);
+			bw.write(findNumber(money)); 
 
 			br.close();
 			bw.close();
@@ -51,8 +51,81 @@ public class Main{
 		Collections.sort(allPrices);
 	}
 	
-	//public static int binarySearch(int number) {
+	public static int binarySearch(int number, int less) {
+		int pos = -1;
+		int i = 0;
+		int j = allPrices.size() - 1;
 		
-	//}
+		while(i <= j && pos < 0) {
+			int half = (i + j)/2;
+			
+			if((allPrices.get(half) + less) == number) {
+				pos = allPrices.get(half);
+
+			}else if((allPrices.get(half) + less) > number) {
+				j = half - 1;
+				
+			}else {
+				i = half + 1;
+			}
+		}
+		return pos;
+	}
+	
+	public static String findNumber(int money) {
+		int result = 0, price1 = 0, price2 = 0, difference1 = 0, difference2 = 2;
+		String message = "";
+		
+		for (int i = 0; i < allPrices.size(); i++) {
+			
+			result = binarySearch(money, allPrices.get(i));
+
+			if(result != -1) {
+				
+				if(price1 == 0 && price2 == 0) {
+					
+					if(result > allPrices.get(i)) {
+						
+						price1 = allPrices.get(i);
+						price2 = result;
+						difference1 = result -  allPrices.get(i);
+						message = ("Peter should buy books whose prices are "+price1+" and "+price2+".");
+						
+					}else {
+						price1 = result;
+						price2 = allPrices.get(i);
+						difference1 = allPrices.get(i) - result;
+						message = ("Peter should buy books whose prices are "+price1+" and "+price2+".");
+					}
+					
+				}else {
+					
+					if(result > allPrices.get(i)) {
+
+						difference2 = result - allPrices.get(i);
+						
+						if(difference1 > difference2) {
+							message = ("Peter should buy books whose prices are "+allPrices.get(i)+" and "+result+".");
+							
+						}else {
+							message = ("Peter should buy books whose prices are "+price1+" and "+price2+".");							
+						}
+
+					}else {
+						difference2 = allPrices.get(i) - result;
+						
+						if(difference1 > difference2) {
+							message = ("Peter should buy books whose prices are "+result+" and "+allPrices.get(i)+".");
+							
+						}else {
+							message = ("Peter should buy books whose prices are "+price1+" and "+price2+".");							
+						}
+					}
+				}
+			}
+
+		}
+		return message;
+	}
 
 }
