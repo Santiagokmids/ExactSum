15c82433c3a8f67ac65974a930c1bb16b0c4be03
Santiago <sntgtrochez@gmail.com>
2021-03-12 21:52:54 -0500

    Exact Sum

--- src/Main.java
+++ src/Main.java
@@ -8,43 +8,41 @@ import java.util.Collections;
 
 public class Main{
 	
-	private final static String SEPARATOR = " "; 
 	private static ArrayList<Integer> allPrices;
+	private final static String SEPARATOR = " ";
+	private static int index = 0;
 	
 	public static void main(String[] args) throws IOException {
 		startApp();
 	}
 	
 	public static void startApp() throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
-
 		String books = br.readLine();
-		String prices = "";
-
-		if(books != null){
+		
+		while(books != null) {
 
 			int cause = Integer.parseInt(books);
-
 			allPrices = new ArrayList<>();
-			prices = br.readLine();
+			String prices = br.readLine();
 			String parts[] = prices.split(SEPARATOR);
-							
+			
 			for (int j = 0; j < cause; j++) {
-
 				allPrices.add(Integer.parseInt(parts[j]));
-
 			}
 			
 			sort();
 			String peterMoney = br.readLine();
 			int money = Integer.parseInt(peterMoney);
-			bw.write(findNumber(money)); 
-
-			br.close();
-			bw.close();
+			bw.write(findNumber(money)+"\n"); 
+			books = br.readLine();
+			books = br.readLine();
 		}
+
+		br.close();
+		bw.close();
 	}
 
 	public static void sort() {
@@ -61,6 +59,7 @@ public class Main{
 			
 			if((allPrices.get(half) + less) == number) {
 				pos = allPrices.get(half);
+				index = half;
 
 			}else if((allPrices.get(half) + less) > number) {
 				j = half - 1;
@@ -84,13 +83,12 @@ public class Main{
 				
 				if(price1 == 0 && price2 == 0) {
 					
-					if(result > allPrices.get(i)) {
-						
+					if(result > allPrices.get(i) && (i-index != 0)) {
 						price1 = allPrices.get(i);
 						price2 = result;
-						difference1 = result -  allPrices.get(i);
+						difference1 = result - allPrices.get(i);
 						message = ("Peter should buy books whose prices are "+price1+" and "+price2+".");
-						
+					
 					}else {
 						price1 = result;
 						price2 = allPrices.get(i);
@@ -100,30 +98,35 @@ public class Main{
 					
 				}else {
 					
-					if(result > allPrices.get(i)) {
-
+					if(result > allPrices.get(i) && (i-index != 0)) {
 						difference2 = result - allPrices.get(i);
 						
 						if(difference1 > difference2) {
+							price1 = allPrices.get(i);
+							price2 = result;
+							difference1 = result - allPrices.get(i);
 							message = ("Peter should buy books whose prices are "+allPrices.get(i)+" and "+result+".");
-							
+
 						}else {
-							message = ("Peter should buy books whose prices are "+price1+" and "+price2+".");							
+							message = ("Peter should buy books whose prices are "+price1+" and "+price2+".");
 						}
 
 					}else {
-						difference2 = allPrices.get(i) - result;
-						
-						if(difference1 > difference2) {
-							message = ("Peter should buy books whose prices are "+result+" and "+allPrices.get(i)+".");
-							
-						}else {
-							message = ("Peter should buy books whose prices are "+price1+" and "+price2+".");							
+						if(result < allPrices.get(i) && (i-index != 0)) {
+							difference2 = allPrices.get(i) - result;
+
+							if(difference1 > difference2) {
+								price1 = result;
+								price2 = allPrices.get(i);
+								difference1 = allPrices.get(i) - result;
+								message = ("Peter should buy books whose prices are "+result+" and "+allPrices.get(i)+".");
+							}else {
+								message = ("Peter should buy books whose prices are "+price1+" and "+price2+".");
+							}
 						}
 					}
 				}
 			}
-
 		}
 		return message;
 	}
